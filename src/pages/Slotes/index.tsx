9b12888566a4946dac660bc8efb506c130830fcd
Odronin <odronin@gmail.com>
2026-02-22 19:00:30 +0700

    Доделал вёрстку экрана выбораного слота

--- src/pages/Slotes/index.tsx
+++ src/pages/Slotes/index.tsx
@@ -1,5 +1,7 @@
 import { Layout } from "components";
 import React from "react";
+import arrowDown from "assets/arrowDown.svg";
+import arrowUp from "assets/arrowUp.svg";
 
 interface ISlot {
     user_id: string;
@@ -78,23 +80,130 @@ const data: ISlot[] = [
 const GameCollapsis: React.FC<ISlotGame> = (game: ISlotGame) => {
     const [isExpanded, setIsExpanded] = React.useState(false);
 
+    const onConnectToGame = (game: ISlotGame) => {
+
+    }
 
     const toggleCollapse = () => {
         setIsExpanded(!isExpanded);
     };
 
-    return (
-        <div className="collapsible-section">
-            {/* The button or div that triggers the collapse on click */}
-            <button onClick={toggleCollapse} className="toggle-button" aria-expanded={isExpanded}>
-                {game.type_title}
-                <span>{isExpanded ? ' ▼' : ' ►'}</span>
-            </button>
+    const colorForLevel = (level: number) => {
+        switch (level) {
+            case 0:
+                return '#22C55E'
+            case 1:
+                return '#FFAB00'
+            case 2:
+                return '#FF5630'
+            default:
+                return 'black'
+        }
+    }
 
-            {/* The content div - visibility controlled by the state */}
-            <div className={`collapse-content ${isExpanded ? 'expanded' : 'collapsed'}`}>
-                <div className="content-inner">
+    const titleForLevel = (level: number) => {
+        switch (level) {
+            case 0:
+                return 'Начальный уровень'
+            case 1:
+                return 'Средний уровень'
+            case 2:
+                return 'Экстерминатус на ножках'
+            default:
+                return ''
+        }
+    }
+
+    return (
+        <div
+            style={{
+                paddingTop: 10,
+                paddingBottom: 10,
+                paddingLeft: 16,
+                paddingRight: 16,
+                backgroundColor: "rgba(145, 158, 171, 0.08)",
+                borderRadius: 12,
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+        >
+            {/* The button or div that triggers the collapse on click */}
+            <button
+                onClick={toggleCollapse}
+                // className="toggle-button"
+                aria-expanded={isExpanded}
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    flex: 1
+                }}
+            >
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flex: 1,
+                        gap: 4,
+                    }}
+                >
+                    {game.type_title}
+                    {!isExpanded ?
+                        <div
+                            style={{
+                                height: 6,
+                                width: 6,
+                                borderRadius: 6,
+                                backgroundColor: colorForLevel(game.level)
+                            }}
+                        />
+                        :
+                        <div />
+                    }
+                </div>
+                <img
+                    src={isExpanded ? arrowUp : arrowDown}
+                    height={3.33 * 2}
+                    width={6.66 * 2}
+                />
+            </button>
+            <div
+                className={`collapse-content ${isExpanded ? 'expanded' : 'collapsed'}`}
+                style={{
+                    gap: 12,
+                }}
+            >
+                <h3
+                    style={{
+                        color: colorForLevel(game.level),
+                        marginTop: 12
+                    }}
+                >
+                    {titleForLevel(game.level)}
+                </h3>
+                <div >
                     {game.describtion}
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flex: 1,
+                        marginTop: 16,
+                        marginBottom: 16
+                    }}
+                >
+                    <button
+                        onClick={() => onConnectToGame(game)}
+                        style={{
+                            flex: 1,
+                            backgroundColor: '#E94817',
+                            height: 48,
+                            padding: 7,
+                            borderRadius: 8
+                        }}
+                    >
+                        Подключиться
+                    </button>
                 </div>
             </div>
         </div>
@@ -108,26 +217,75 @@ export const Slot: React.FC = () => {
                 <h1 className="text-lg text-primary-text font-semibold">
                     13 февраля - Вечер
                 </h1>
-                {
-                    data.map((autor) => {
-                        return (
-                            <section>
-                                <div>
-                                    <img src={autor.avatar} width={48} height={48}/>
-                                    <h2>{autor.username}</h2>
-                                </div>
-                                <section>
-                                    {autor.games.map((gameSlot) => {
-                                        return (
-                                            <GameCollapsis {...gameSlot} />
-                                        )
-                                    })}
-                                </section>
-                            </section>
-                        )
-                    })
-                }
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 16
+                    }}
+                >
+                    {
+                        data.map((autor) => {
+                            return (
+                                <section
+                                    style={{
+                                        backgroundColor: "#26303A",
+                                        padding: 16,
+                                        gap: 16,
+                                        borderRadius: 8,
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                    }}
+                                >
+                                    <div
+                                        style={{
+                                            display: 'flex',
+                                            flexDirection: 'row',
+                                            gap: 14
+                                        }}
+                                    >
+                                        <img
+                                            src={autor.avatar}
+                                            width={48}
+                                            height={48}
+                                            style={{
+                                                borderRadius: 24,
+                                                height: 48,
+                                                objectFit: 'contain'
+                                            }}
 
+                                        />
+                                        <div
+                                            style={{
+                                                display: 'flex',
+                                                alignItems: 'center'
+                                            }}
+                                        >
+                                            <h2
+
+                                            >
+                                                {autor.username}
+                                            </h2>
+                                        </div>
+                                    </div>
+                                    <section
+                                        style={{
+                                            display: 'flex',
+                                            gap: 8,
+                                            flexDirection: 'column'
+                                        }}
+                                    >
+                                        {autor.games.map((gameSlot) => {
+                                            return (
+                                                <GameCollapsis {...gameSlot} />
+                                            )
+                                        })}
+                                    </section>
+                                </section>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </Layout>
     );
