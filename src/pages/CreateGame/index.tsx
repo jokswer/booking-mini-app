c02d321eb62639bc16aed46d2d7db7423f1fd526
Odronin <odronin@gmail.com>
2026-03-29 01:56:04 +0700

    Подготовка экрана создания игры

--- src/pages/CreateGame/index.tsx
+++ src/pages/CreateGame/index.tsx
@@ -3,28 +3,89 @@ import { BackButton } from "@twa-dev/sdk/react";
 
 import { GameSystemsSellercot, Layout, Modal, AddedTimeSlote } from "components";
 import { useAppNavigation } from "navigation";
+import plusIcon from 'assets/plus.svg';
 
 export const CreateGamePage = () => {
 
-  const [showModal, setShowModal] = React.useState<boolean>(false)
+  const [showModalTimeSlote, setShowModalTimeSlote] = React.useState<boolean>(false)
+  const [showModalGameSystems, setShowModalGameSystems] = React.useState<boolean>(false)
 
   const { goBack } = useAppNavigation();
   return (
     <Layout>
-      Создать игру
       <BackButton onClick={goBack} />
-      <button
-        onClick={() => setShowModal(true)}
+      <div
+        style={{
+          gap: 24,
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1
+        }}
       >
-        Выбрать типы игры
-      </button>
+        <p
+          style={{
+            fontSize: 20,
+            fontWeight: '700',
+            verticalAlign: 'middle',
+
+          }}
+        >
+          Создать игру
+        </p>
+        <div
+          style={{
+            gap: 12,
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <p style={styles.title} >
+            Игровые системы
+          </p>
+          <div>
+            <button
+              style={styles.button}
+              onClick={() => setShowModalGameSystems(true)}
+            >
+              <img src={plusIcon} width={24} height={24} />
+              <p>
+                Добавить игровую систему
+              </p>
+
+            </button></div>
+        </div>
+        <div
+          style={{
+            gap: 12,
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <p style={styles.title} >
+            Желаемое время игры
+          </p>
+          <div>
+            <button
+              style={styles.button}
+              onClick={() => setShowModalGameSystems(true)}
+            >
+              <img src={plusIcon} width={24} height={24} />
+              <p>
+                Добавить дату
+              </p>
+
+            </button>
+          </div>
+        </div>
+
+      </div>
       <Modal
         onClose={() => {
-          setShowModal(false)
+          setShowModalGameSystems(false)
         }}
-        isOpen={showModal}
+        isOpen={showModalGameSystems}
       >
-        {/* <GameSystemsSellercot
+        <GameSystemsSellercot
           types={[{
             typesId: "1",
             shortName: "wh 40k",
@@ -41,9 +102,16 @@ export const CreateGamePage = () => {
             fullName: "Age of Sigmar"
           }]}
           selected={[]}
-          onClose={() => setShowModal(false)}
-          onSelect={(types) => setShowModal(false)}
-        /> */}
+          onClose={() => setShowModalGameSystems(false)}
+          onSelect={(types) => setShowModalGameSystems(false)}
+        />
+      </Modal>
+      <Modal
+        onClose={() => {
+          setShowModalTimeSlote(false)
+        }}
+        isOpen={showModalTimeSlote}
+      >
         <AddedTimeSlote
           date={'10 февраля'}
           dayWeek={'Чт'}
@@ -61,13 +129,34 @@ export const CreateGamePage = () => {
           ]}
           selected={["1", "2"]}
           onSelect={() => {
-
+            setShowModalTimeSlote(false)
           }}
           onDelete={() => {
-
+            setShowModalTimeSlote(false)
           }}
         />
       </Modal>
     </Layout>
   );
 };
+
+const styles = {
+  button: {
+    display: 'flex',
+    alignItems: "center",
+    padding: 12,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#919EAB52"
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: '600',
+    verticalAlign: 'middle',
+  },
+  blokContainer: {
+    gap: 12,
+    display: 'flex',
+    flexDirection: 'column',
+  }
+}
